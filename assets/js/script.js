48203097e55c506f463058033b17ffd98c1487e1
Edgard <edgardurzuafaundez@gmail.com>
2024-07-07 20:01:44 -0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,69 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const contenedorVenta = document.getElementById('propiedades_venta');
+    const contenedorAlquiler = document.getElementById('propiedades_alquiler');
+
+    propiedadesVenta.forEach(propiedad => {
+        const propiedadCard = crearCardPropiedad(propiedad);
+        contenedorVenta.appendChild(propiedadCard);
+    });
+
+    propiedadesAlquiler.forEach(propiedad => {
+        const propiedadCard = crearCardPropiedad(propiedad);
+        contenedorAlquiler.appendChild(propiedadCard);
+    });
+});
+
+function crearCardPropiedad(propiedad) {
+    const col = document.createElement('div');
+    col.classList.add('col-md-4');
+
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const img = document.createElement('img');
+    img.src = propiedad.img;
+    img.classList.add('card-img-top');
+    img.alt = `Imagen de ${propiedad.title}`;
+
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+
+    const title = document.createElement('h5');
+    title.classList.add('card-title');
+    title.innerText = propiedad.title;
+
+    const description = document.createElement('p');
+    description.classList.add('card-text');
+    description.innerText = propiedad.description;
+
+    const location = document.createElement('p');
+    location.innerHTML = `<i class="fas fa-map-marker-alt"></i> ${propiedad.location}`;
+
+    const roomsAndBathrooms = document.createElement('p');
+    roomsAndBathrooms.innerHTML = `<i class="fas fa-bed"></i> ${propiedad.rooms} Habitaciones | <i class="fas fa-bath"></i> ${propiedad.bathrooms} Baños`;
+
+    const price = document.createElement('p');
+    price.innerHTML = `<i class="fas fa-dollar-sign"></i> ${propiedad.price}`;
+
+    const smoking = document.createElement('p');
+    smoking.classList.add(propiedad.smokingAllowed ? 'text-success' : 'text-danger');
+    smoking.innerHTML = `<i class="fas fa-smoking"></i> ${propiedad.smokingAllowed ? 'Permitido fumar' : 'No se permite fumar'}`;
+
+    const pets = document.createElement('p');
+    pets.classList.add(propiedad.petsAllowed ? 'text-success' : 'text-danger');
+    pets.innerHTML = `<i class="fas fa-paw"></i> ${propiedad.petsAllowed ? 'Mascotas permitidas' : 'No se permiten mascotas'}`;
+
+    cardBody.appendChild(title);
+    cardBody.appendChild(description);
+    cardBody.appendChild(location);
+    cardBody.appendChild(roomsAndBathrooms);
+    cardBody.appendChild(price);
+    cardBody.appendChild(smoking);
+    cardBody.appendChild(pets);
+
+    card.appendChild(img);
+    card.appendChild(cardBody);
+    col.appendChild(card);
+
+    return col;
+}
---
+++ assets/js/script_venta.js
@@ -0,0 +1,68 @@
+const propiedadesVenta = [
+    {
+        img: "https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg",
+        title: "Apartamento de lujo en zona exclusiva",
+        description: "Este apartamento de lujo está ubicado en una exclusiva zona  residencial",
+        location: "123 Luxury Lane, Prestige Suburb, CA 45678",
+        rooms: 4,
+        bathrooms: 4,
+        price: 5000,
+        smokingAllowed: false,
+        petsAllowed: false
+    },
+    {
+        img: "https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg",
+        title: "Apartamento acogedor en la montaña",
+        description: "Este apartamento acogedor está situado en lo alto de una         montaña con impresionantes vistas",
+        location: "789 Mountain Road, Summit Peaks, CA 23456",
+        rooms: 2,
+        bathrooms: 1,
+        price: 1200,
+        smokingAllowed: true,
+        petsAllowed: true
+    },
+    {
+        img: "https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg",
+        title: "Penthouse de lujo con terraza panorámica",
+        description: "Este penthouse de lujo ofrece una terraza panorámica con vistas espectaculares",
+        location: "567 Skyline Avenue,Skyview City, CA 56789",
+        rooms: 3,
+        bathrooms: 3,
+        price: 4500,
+        smokingAllowed: false,
+        petsAllowed: true
+    },
+    {
+        img: "https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg",
+        title: "Penthouse de lujo con terraza panorámica",
+        description: "Este penthouse de lujo ofrece una terraza panorámica con vistas espectaculares",
+        location: "567 Skyline Avenue,Skyview City, CA 56789",
+        rooms: 3,
+        bathrooms: 3,
+        price: 4500,
+        smokingAllowed: false,
+        petsAllowed: true
+    },
+    {
+        img: "https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg",
+        title: "Penthouse de lujo con terraza panorámica",
+        description: "Este penthouse de lujo ofrece una terraza panorámica con vistas espectaculares",
+        location: "567 Skyline Avenue,Skyview City, CA 56789",
+        rooms: 3,
+        bathrooms: 3,
+        price: 4500,
+        smokingAllowed: false,
+        petsAllowed: true
+    },
+    {
+        img: "https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg",
+        title: "Penthouse de lujo con terraza panorámica",
+        description: "Este penthouse de lujo ofrece una terraza panorámica con vistas espectaculares",
+        location: "567 Skyline Avenue,Skyview City, CA 56789",
+        rooms: 3,
+        bathrooms: 3,
+        price: 4500,
+        smokingAllowed: false,
+        petsAllowed: true
+    }
+];
